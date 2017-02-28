6051046d6eb866bbe34d9e6cd423c00cbebd6a9b
Chet Corcos <chet.corcos@affirm.com>
2017-02-27 21:39:15 -0800

    dev server working

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: 'dist',
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
     filename: 'bundle.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'chetcorcos.com',
+    }),
+  ],
 }
