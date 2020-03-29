5d77b6f76f36af624c78610281eca93323467be8
Chet Corcos <ccorcos@gmail.com>
2020-03-28 21:19:33 -0700

    Upgrades

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,5 @@
 import * as path from "path"
-import * as HtmlWebpackPlugin from "html-webpack-plugin"
+import HtmlWebpackPlugin from "html-webpack-plugin"
 import { Configuration } from "webpack"
 
 const config: Configuration = {
