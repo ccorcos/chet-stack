b3ecc3604560d36094d937fdd1f66ee416a6863f
Chet Corcos <ccorcos@gmail.com>
2023-06-30 12:23:02 -0700

    Fix statusCode for errors

--- src/shared/errors.ts
+++ src/shared/errors.ts
@@ -1,12 +1,12 @@
 // These custom errors should never contain sensitive information in the message.
 export class ValidationError extends Error {
-	static statusCode = 400
+	statusCode = 400
 }
 
 export class TransactionConflictError extends Error {
-	static statusCode = 409
+	statusCode = 409
 }
 
 export class BrokenError extends Error {
-	static statusCode = 500
+	statusCode = 500
 }
