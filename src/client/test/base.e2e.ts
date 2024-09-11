8ceeec97d0e233c3af7f99e264048fc2f5742c1e
Chet Corcos <ccorcos@gmail.com>
2024-09-11 10:50:55 -0700

    e2e test

--- src/client/test/base.e2e.ts
+++ src/client/test/base.e2e.ts
@@ -19,48 +19,9 @@ async function click(page: Page, selector: string) {
 	return button
 }
 
-async function login(page: Page, username: string, password: string) {
-	await page.goto("/login")
-	await write(page, ".login input.username", username)
-	await write(page, '.login input[type="password"]', password)
-	await click(page, '.login button[type="submit"]')
-}
-
-async function signup(page: Page, username: string, password: string) {
-	await page.goto("/login")
-	await write(page, ".signup input.username", username)
-	await write(page, '.signup input[type="password"]', password)
-	await click(page, '.signup button[type="submit"]')
-}
-
-async function logout(page: Page) {
-	await page.goto("/logout")
-}
-
 test("Smoke Test", async ({ browser }) => {
-	const chetContext = await browser.newContext()
-	const samContext = await browser.newContext()
-	const chet = await chetContext.newPage()
-	const sam = await samContext.newPage()
-
-	await chet.goto("/")
-	await sam.goto("/")
-
-	await signup(chet, "chet", "1234")
-	await signup(sam, "sam", "1234")
-
-	await click(sam, 'button:text("New Thread")')
-
-	await write(sam, 'input[placeholder="Add member..."]', "ch")
-	await waitFor(sam, '.popup > .menu-item:has-text("chet")')
-	await sam.keyboard.press("Enter")
-
-	await write(sam, "input.subject", "Friends")
-
-	await click(chet, '*[role="listitem"] > *:has-text("Friends")')
-
-	await write(sam, "input.reply", "Hello there!")
-	await sam.keyboard.press("Enter")
-
-	await waitFor(chet, '.message > *:has-text("Hello there!")')
+	const context = await browser.newContext()
+	const page = await context.newPage()
+	await page.goto("/")
+	await waitFor(page, "body")
 })
