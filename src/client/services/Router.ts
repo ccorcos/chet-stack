cb7ebe9c13e1a8bf7fd06179ca9d2d828d02ba8f
Chet Corcos <ccorcos@gmail.com>
2024-12-09 12:19:05 -0800

    Decouple the Router from hardcoded Route types.

--- src/client/services/Router.ts
+++ src/client/services/Router.ts
@@ -1,13 +1,26 @@
 import { useEffect, useState } from "react"
-import { Route, formatRoute, parseRoute } from "../../shared/routeHelpers"
 import { useClientEnvironment } from "./ClientEnvironment"
+
+const ORIGIN = window.location.origin
+
+function isExternalLink(url: string) {
+	if (url.startsWith("/")) return false
+	if (url.startsWith(ORIGIN)) return false
+	return true
+}
+
+function getRelativeUrl(link: string) {
+	if (isExternalLink(link)) throw new Error("Cannot get relative URL of external link")
+	if (link.startsWith("/")) return link
+	return link.slice(ORIGIN.length)
+}
 
 // HistoryState is currently unused. This is where you would save stuff like scroll
 // position or selection so you can restore when navigating back.
 export type HistoryState = {}
 
 export type RouterState = {
-	route: Route
+	route: string
 	historyState: HistoryState | undefined
 }
 
@@ -15,15 +28,14 @@ export class Router {
 	state: RouterState
 
 	constructor() {
+		const route = getRelativeUrl(window.location.href)
 		const historyState: HistoryState | undefined = window.history.state
-		const route = parseRoute(window.location.href)
 		this.state = { route, historyState }
 		window.onpopstate = this.onPopState
 	}
 
 	private onPopState = (event: PopStateEvent) => {
-		const url = window.location.href
-		const route = parseRoute(url)
+		const route = getRelativeUrl(window.location.href)
 		const historyState: HistoryState | undefined = event.state
 		this.setState({ historyState, route })
 	}
@@ -43,10 +55,24 @@ export class Router {
 		}
 	}
 
-	navigate = (route: Route) => {
+	navigate = (link: string) => {
+		// Open external links in a new tab.
+		if (isExternalLink(link)) return window.open(link, "_blank")
+
+		const route = getRelativeUrl(link)
 		const historyState = undefined
-		window.history.pushState(historyState, "", formatRoute(route))
+		window.history.pushState(historyState, "", route)
 		this.setState({ historyState, route })
+	}
+
+	setParam = (key: string, value: string) => {
+		const url = new URL(window.location.href)
+		url.searchParams.set(key, value)
+		const route = getRelativeUrl(url.toString())
+
+		const historyState = undefined
+		window.history.replaceState(historyState, "", route)
+		this.setState({ route, historyState })
 	}
 
 	back = () => {
