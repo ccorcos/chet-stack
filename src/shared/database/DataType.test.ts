8fbf08b6f452e51d00fd3ae822895500edca1f97
Chet Corcos <ccorcos@gmail.com>
2025-04-15 12:01:29 -0700

    DataType

---
+++ src/shared/database/DataType.test.ts
@@ -0,0 +1,212 @@
+import { strict as assert } from "assert"
+import { describe, it } from "mocha"
+import { Assert } from "../typeHelpers"
+import * as t from "./DataType"
+import {
+	ArrayDataType,
+	BooleanDataType,
+	InferType,
+	LiteralDataType,
+	MapDataType,
+	NullDataType,
+	NumberDataType,
+	ObjectDataType,
+	Optional,
+	OrDataType,
+	StringDataType,
+	TupleDataType,
+	UndefinedDataType,
+} from "./DataType"
+
+// InferType tests.
+type InferTests =
+	| Assert<InferType<NullDataType>, null>
+	| Assert<InferType<UndefinedDataType>, undefined>
+	| Assert<InferType<StringDataType>, string>
+	| Assert<InferType<NumberDataType>, number>
+	| Assert<InferType<BooleanDataType>, boolean>
+	| Assert<InferType<LiteralDataType<12>>, 12>
+	| Assert<InferType<LiteralDataType<"hello">>, "hello">
+	| Assert<InferType<LiteralDataType<true>>, true>
+	| Assert<InferType<LiteralDataType<string>>, string>
+	| Assert<InferType<ArrayDataType<StringDataType>>, Array<string>>
+	| Assert<InferType<ArrayDataType<NumberDataType>>, Array<number>>
+	| Assert<
+			InferType<TupleDataType<[LiteralDataType<"hello">, StringDataType, NumberDataType]>>,
+			["hello", string, number]
+	  >
+	| Assert<InferType<MapDataType<StringDataType>>, { [key: string]: string }>
+	| Assert<InferType<MapDataType<NumberDataType>>, { [key: string]: number }>
+	| Assert<
+			InferType<
+				ObjectDataType<{
+					a: LiteralDataType<"hello">
+					b: StringDataType
+					c: Optional<NumberDataType>
+				}>
+			>,
+			{ a: "hello"; b: string; c?: number }
+	  >
+	| Assert<
+			// @ts-expect-error
+			InferType<
+				ObjectDataType<{
+					a: LiteralDataType<"hello">
+					b: StringDataType
+					c: Optional<NumberDataType>
+				}>
+			>,
+			{ a: "hello"; b: string; c: number }
+	  >
+	| Assert<
+			InferType<OrDataType<LiteralDataType<true> | StringDataType | NumberDataType>>,
+			true | string | number
+	  >
+
+describe("DataType", () => {
+	const valid = (dt: t.DataType, value: any) => {
+		const result = t.validate(dt, value)
+		assert.equal(!result, true, result ? t.formatError(result) : "")
+	}
+	const invalid = (dt: t.DataType, value: any) => {
+		const result = t.validate(dt, value)
+		assert.equal(!!result, true, result ? t.formatError(result) : "")
+	}
+
+	it("validate", () => {
+		// String validation
+		valid(t.string, "hello")
+		valid(t.string, "")
+		invalid(t.string, 123)
+		invalid(t.string, true)
+		invalid(t.string, null)
+		invalid(t.string, undefined)
+
+		// Number validation
+		valid(t.number, 123)
+		valid(t.number, 0)
+		valid(t.number, -1.5)
+		invalid(t.number, "123")
+		invalid(t.number, true)
+		invalid(t.number, null)
+
+		// Boolean validation
+		valid(t.boolean, true)
+		valid(t.boolean, false)
+		invalid(t.boolean, "true")
+		invalid(t.boolean, 1)
+		invalid(t.boolean, null)
+
+		// Null validation
+		valid(t.null_, null)
+		invalid(t.null_, undefined)
+		invalid(t.null_, "null")
+		invalid(t.null_, 0)
+
+		// Undefined validation
+		valid(t.undefined_, undefined)
+		invalid(t.undefined_, null)
+		invalid(t.undefined_, "undefined")
+
+		// Any validation
+		valid(t.any, null)
+		valid(t.any, undefined)
+		valid(t.any, "hello")
+		valid(t.any, 123)
+		valid(t.any, true)
+		valid(t.any, false)
+		valid(t.any, [])
+		valid(t.any, {})
+
+		// Literal validation
+		valid(t.literal("hello"), "hello")
+		invalid(t.literal("hello"), "world")
+		valid(t.literal(42), 42)
+		invalid(t.literal(42), 43)
+		valid(t.literal(true), true)
+		invalid(t.literal(true), false)
+
+		// Array validation
+		valid(t.array(t.string), ["hello", "world"])
+		invalid(t.array(t.string), [123, "world"])
+		invalid(t.array(t.string), { a: "hello", b: "world" })
+		invalid(t.array(t.string), true)
+		invalid(t.array(t.string), null)
+		invalid(t.array(t.string), undefined)
+		valid(t.array(t.or(t.number, t.string)), [])
+		valid(t.array(t.or(t.number, t.string)), [123])
+		valid(t.array(t.or(t.number, t.string)), ["123"])
+		valid(t.array(t.or(t.number, t.string)), ["123", 123])
+		invalid(t.array(t.or(t.number, t.string)), ["123", 123, true])
+
+		// Tuple validation
+		valid(t.tuple(t.string, t.number), ["hello", 123])
+		invalid(t.tuple(t.string, t.number), ["hello", 123, true])
+		invalid(t.tuple(t.string, t.number), [123, "hello"])
+		invalid(t.tuple(t.string, t.number), { a: "hello", b: 123 })
+		invalid(t.tuple(t.string, t.number), true)
+		invalid(t.tuple(t.string, t.number), null)
+		invalid(t.tuple(t.string, t.number), undefined)
+
+		// Map validation
+		valid(t.map(t.string), { a: "hello", b: "world" })
+		invalid(t.map(t.string), { a: "hello", b: 123 })
+		invalid(t.map(t.string), [123, "hello"])
+		invalid(t.map(t.string), true)
+		invalid(t.map(t.string), null)
+		invalid(t.map(t.string), undefined)
+
+		// Or validation
+		valid(t.or(t.string, t.number), "hello")
+		valid(t.or(t.string, t.number), 123)
+		invalid(t.or(t.string, t.number), true)
+		invalid(t.or(t.string, t.number), null)
+		invalid(t.or(t.string, t.number), undefined)
+
+		// Object validation
+		const user = t.object({
+			id: t.string,
+			age: t.optional(t.number),
+		})
+
+		type User = InferType<typeof user>
+		type Test = Assert<User, { id: string; age?: number }>
+		// @ts-expect-error
+		type MissingOptional = Assert<User, { id: string; age: number }>
+		// @ts-expect-error
+		type MissiontOptionalUndefined = Assert<User, { id: string; age: number | undefined }>
+
+		assert.deepEqual(user, {
+			type: "object",
+			properties: {
+				id: { type: "string" },
+				age: { type: "optional", value: { type: "number" } },
+			},
+		})
+
+		valid(user, { id: "123", age: 12 })
+		valid(user, { id: "123" })
+		invalid(user, { id: "123", age: "hello" })
+		invalid(user, { age: "hello" })
+	})
+
+	it("is", () => {
+		// Test is() function
+		let thing: any
+		assert.equal(t.is(t.string, thing), false)
+		if (t.is(t.string, thing)) {
+			type ThingIsString = Assert<typeof thing, string>
+		}
+	})
+
+	it("dataTypeDataType", () => {
+		const ThingType = t.object({
+			a: t.string,
+			b: t.optional(t.number),
+			c: t.or(t.boolean, t.null_, t.undefined_),
+			d: t.array(t.any),
+			e: t.map(t.tuple(t.string, t.number)),
+		})
+		valid(t.dataTypeDataType, ThingType)
+	})
+})
---
+++ src/shared/database/DataType.ts
@@ -0,0 +1,463 @@
+import isBoolean from "lodash/isBoolean"
+import isEqual from "lodash/isEqual"
+import isNumber from "lodash/isNumber"
+import isPlainObject_ from "lodash/isPlainObject"
+import isString from "lodash/isString"
+import { Simplify } from "../typeHelpers"
+
+// ============================================================================
+// Data Types
+// ============================================================================
+
+export interface NullDataType {
+	type: "null"
+}
+
+export interface UndefinedDataType {
+	type: "undefined"
+}
+
+export interface StringDataType {
+	type: "string"
+}
+
+export interface NumberDataType {
+	type: "number"
+}
+
+export interface BooleanDataType {
+	type: "boolean"
+}
+
+export interface LiteralDataType<T extends string | number | boolean> {
+	type: "literal"
+	value: T
+}
+
+export interface ArrayDataType<T extends DataType> {
+	type: "array"
+	items: T
+}
+
+export interface TupleDataType<T extends DataType[]> {
+	type: "tuple"
+	items: T
+}
+
+export interface MapDataType<T extends DataType> {
+	type: "map"
+	items: T
+}
+
+export type Optional<T extends DataType> = { type: "optional"; value: T }
+
+export interface ObjectDataType<T extends { [key: string]: DataType | Optional<DataType> }> {
+	type: "object"
+	properties: T
+}
+
+export interface AnyDataType {
+	type: "any"
+}
+
+export interface OrDataType<T extends DataType> {
+	type: "or"
+	options: Array<T>
+}
+
+export type DataType =
+	| NullDataType
+	| UndefinedDataType
+	| StringDataType
+	| NumberDataType
+	| BooleanDataType
+	| AnyDataType
+	| LiteralDataType<string | number | boolean>
+	| ArrayDataType<DataType>
+	| TupleDataType<DataType[]>
+	| MapDataType<DataType>
+	| ObjectDataType<{ [key: string]: DataType | Optional<DataType> }>
+	| OrDataType<DataType>
+
+// ============================================================================
+// Type Inference
+// ============================================================================
+
+type PickRequired<T extends { [key: string]: DataType | Optional<DataType> }> = {
+	[P in keyof T as T[P] extends DataType ? P : never]: T[P] extends DataType
+		? InferType<T[P]>
+		: never
+}
+type PickOptional<T extends { [key: string]: DataType | Optional<DataType> }> = {
+	[P in keyof T as T[P] extends Optional<infer U> ? P : never]?: T[P] extends Optional<infer U>
+		? InferType<U>
+		: never
+}
+
+type InferObject<T extends { [key: string]: DataType | Optional<DataType> }> = Simplify<
+	PickRequired<T> & PickOptional<T>
+>
+
+export type InferType<T extends DataType> = T extends NullDataType
+	? null
+	: T extends UndefinedDataType
+	? undefined
+	: T extends StringDataType
+	? string
+	: T extends NumberDataType
+	? number
+	: T extends BooleanDataType
+	? boolean
+	: T extends LiteralDataType<infer U>
+	? U
+	: T extends ArrayDataType<infer U>
+	? Array<InferType<U>>
+	: T extends TupleDataType<infer U>
+	? { [K in keyof U]: InferType<U[K]> }
+	: T extends MapDataType<infer U>
+	? { [key: string]: InferType<U> }
+	: T extends ObjectDataType<infer U>
+	? InferObject<U>
+	: T extends OrDataType<infer U>
+	? InferType<U>
+	: never
+
+// ============================================================================
+// Construction Helpers.
+// ============================================================================
+
+export const null_: NullDataType = { type: "null" }
+export const undefined_: UndefinedDataType = { type: "undefined" }
+export const string: StringDataType = { type: "string" }
+export const number: NumberDataType = { type: "number" }
+export const boolean: BooleanDataType = { type: "boolean" }
+export const any: AnyDataType = { type: "any" }
+
+export function literal<T extends string | number | boolean>(value: T): LiteralDataType<T> {
+	return { type: "literal", value }
+}
+
+export function array<T extends DataType>(inner: T): ArrayDataType<T> {
+	return { type: "array", items: inner }
+}
+
+export function tuple<T extends DataType[]>(...values: T): TupleDataType<T> {
+	return { type: "tuple", items: values }
+}
+
+export function map<T extends DataType>(inner: T): MapDataType<T> {
+	return { type: "map", items: inner }
+}
+
+export function optional<T extends DataType>(value: T): Optional<T> {
+	return { type: "optional", value }
+}
+
+export function object<T extends { [key: string]: DataType | Optional<DataType> }>(
+	properties: T
+): ObjectDataType<T> {
+	return { type: "object", properties }
+}
+
+export function or<T extends DataType[]>(...values: T): OrDataType<T[number]> {
+	return { type: "or", options: values }
+}
+
+// ============================================================================
+// Validation.
+// ============================================================================
+
+function isPlainObject(obj: unknown): obj is object {
+	return isPlainObject_(obj)
+}
+
+export type ValidateError = {
+	message: string
+	path: Array<number | string>
+	children?: Array<ValidateError>
+}
+
+type Validator<T extends DataType["type"]> = (
+	dataType: Extract<DataType, { type: T }>,
+	value: any
+) => ValidateError | undefined
+
+/** A map of DataType.type to validator functions. */
+const Validators: {
+	[K in DataType["type"]]: Validator<K>
+} = {
+	undefined: (dataType, value) => {
+		if (value !== undefined) {
+			return {
+				message: `${JSON.stringify(value)} is not undefined`,
+				path: [],
+			}
+		}
+	},
+	null: (dataType, value) => {
+		if (value !== null) {
+			return {
+				message: `${JSON.stringify(value)} is not null`,
+				path: [],
+			}
+		}
+	},
+	string: (dataType, value) => {
+		if (!isString(value)) {
+			return {
+				message: `${JSON.stringify(value)} is not a string`,
+				path: [],
+			}
+		}
+	},
+	number: (dataType, value) => {
+		if (!isNumber(value)) {
+			return {
+				message: `${JSON.stringify(value)} is not a number`,
+				path: [],
+			}
+		}
+	},
+	boolean: (dataType, value) => {
+		if (!isBoolean(value)) {
+			return {
+				message: `${JSON.stringify(value)} is not a boolean`,
+				path: [],
+			}
+		}
+	},
+	any: (dataType, value) => undefined,
+	literal: (dataType, value) => {
+		if (!isEqual(value, dataType.value)) {
+			return {
+				message: `${JSON.stringify(value)} is not ${JSON.stringify(dataType.value)}`,
+				path: [],
+			}
+		}
+	},
+	array: (dataType, value) => {
+		if (!Array.isArray(value)) {
+			return {
+				message: `${JSON.stringify(value)} is not an array`,
+				path: [],
+			}
+		}
+		for (let i = 0; i < value.length; i++) {
+			const error = validate(dataType.items, value[i])
+			if (error) {
+				return {
+					...error,
+					path: [i, ...error.path],
+				}
+			}
+		}
+	},
+	tuple: (dataType, value) => {
+		if (!Array.isArray(value)) {
+			return {
+				message: `${JSON.stringify(value)} is not an array`,
+				path: [],
+			}
+		}
+		if (value.length !== dataType.items.length) {
+			return {
+				message: `${JSON.stringify(value)} is not a tuple of length ${dataType.items.length}`,
+				path: [],
+			}
+		}
+		for (let i = 0; i < dataType.items.length; i++) {
+			const error = validate(dataType.items[i], value[i])
+			if (error) {
+				return {
+					...error,
+					path: [i, ...error.path],
+				}
+			}
+		}
+	},
+	map: (dataType, value) => {
+		if (!isPlainObject(value)) {
+			return {
+				message: `${JSON.stringify(value)} is not a map`,
+				path: [],
+			}
+		}
+		for (const key in value) {
+			const error = validate(dataType.items, value[key])
+			if (error) {
+				return {
+					...error,
+					path: [key, ...error.path],
+				}
+			}
+		}
+	},
+	object: (dataType, value) => {
+		if (!isPlainObject(value)) {
+			return {
+				message: `${JSON.stringify(value)} is not an object`,
+				path: [],
+			}
+		}
+		for (const key in dataType.properties) {
+			let dt: DataType
+			if (dataType.properties[key].type === "optional") {
+				dt = dataType.properties[key].value
+				if (dt.type === "or") {
+					// If the existing type is a union, then just add undefined as an option.
+					if (!dt.options.some((t) => t.type === "undefined")) {
+						dt = or(undefined_, ...dt.options)
+					}
+				} else {
+					dt = or(undefined_, dt)
+				}
+			} else {
+				dt = dataType.properties[key]
+			}
+			const error = validate(dt, value[key])
+			if (error) {
+				return {
+					...error,
+					path: [key, ...error.path],
+				}
+			}
+		}
+	},
+	or: (dataType, value) => {
+		const errors: Array<ValidateError> = []
+		for (const dt of dataType.options) {
+			const error = validate(dt, value)
+			if (!error) return
+			errors.push(error)
+		}
+		// TODO: find discriminating keys so we can report just one message.
+		return {
+			message: `${JSON.stringify(value)} must satisfy one of:`,
+			path: [],
+			children: errors,
+		}
+	},
+}
+
+export function validate<T extends DataType>(dataType: T, value: any): ValidateError | undefined {
+	const validator = Validators[dataType.type]
+	return validator(dataType as any, value)
+}
+
+export function is<T extends DataType>(dataType: T, value: any): value is InferType<T> {
+	return !validate(dataType, value)
+}
+
+function pathToString(path: Array<string | number>) {
+	return path
+		.map((item) => {
+			if (isNumber(item)) {
+				return `[${item}]`
+			}
+			if (/^[a-zA-Z][a-zA-Z0-9]*$/.test(item)) {
+				return `.${item}`
+			}
+			return `[${JSON.stringify(item)}]`
+		})
+		.join("")
+}
+
+function indent(str: string) {
+	return "  " + str.split("\n").join("\n  ")
+}
+
+export function formatError(error: ValidateError) {
+	let str = ""
+	if (error.path.length) {
+		str += pathToString(error.path)
+		str += ": "
+	}
+	str += error.message
+	if (error.children) {
+		str += "\n"
+		str += indent(error.children.map(formatError).join("\n"))
+	}
+	return str
+}
+
+// ============================================================================
+// Inspection.
+// ============================================================================
+
+type Inspector<T extends DataType["type"]> = (dataType: Extract<DataType, { type: T }>) => string
+
+/** A map of DataType.type to validator functions. */
+const Inspectors: {
+	[K in DataType["type"]]: Inspector<K>
+} = {
+	null: (dataType) => "null",
+	undefined: (dataType) => "undefined",
+	string: (dataType) => "string",
+	number: (dataType) => "number",
+	boolean: (dataType) => "boolean",
+	literal: (dataType) => JSON.stringify(dataType.value),
+	array: (dataType) => "Array<" + inspect(dataType.items) + ">",
+	tuple: (dataType) => "[" + dataType.items.map(inspect).join(", ") + "]",
+	map: (dataType) => "{ [key: string]: " + inspect(dataType.items) + " }",
+	object: (dataType) =>
+		"{ " +
+		[
+			...Object.keys(dataType.properties).map((key) => {
+				const property = dataType.properties[key]
+				const isOptional = property.type === "optional"
+				const propertyToInspect = isOptional ? property.value : property
+				return key + (isOptional ? "?: " : ": ") + inspect(propertyToInspect)
+			}),
+		].join("; ") +
+		" }",
+	any: (dataType) => "any",
+	or: (dataType) => dataType.options.map(inspect).join(" | "),
+}
+
+export function inspect<T extends DataType>(dataType: T): string {
+	return Inspectors[dataType.type](dataType as any)
+}
+
+// ============================================================================
+// DataType DataType.
+// ============================================================================
+
+// We're going to mutate this array to avoid circular references.
+export const dataTypeDataType: OrDataType<DataType> = { type: "or", options: [] }
+
+const dataTypeDataTypes: { [K in DataType["type"]]: DataType } = {
+	null: object({ type: literal("null") }),
+	undefined: object({ type: literal("undefined") }),
+	string: object({ type: literal("string") }),
+	number: object({ type: literal("number") }),
+	boolean: object({ type: literal("boolean") }),
+	any: object({ type: literal("any") }),
+	literal: object({
+		type: literal("literal"),
+		value: or(string, number, boolean),
+	}),
+	array: object({
+		type: literal("array"),
+		items: dataTypeDataType,
+	}),
+	tuple: object({
+		type: literal("tuple"),
+		items: array(dataTypeDataType),
+	}),
+	map: object({
+		type: literal("map"),
+		items: dataTypeDataType,
+	}),
+	object: object({
+		type: literal("object"),
+		properties: map(
+			or(dataTypeDataType, object({ type: literal("optional"), value: dataTypeDataType }))
+		),
+	}),
+	or: object({
+		type: literal("or"),
+		options: array(dataTypeDataType),
+	}),
+}
+
+for (const value of Object.values(dataTypeDataTypes)) dataTypeDataType.options.push(value)
