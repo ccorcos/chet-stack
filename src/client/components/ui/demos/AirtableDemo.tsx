6b5682b562a58de6b1fbe94ac7d51fda9e1fa11d
Chet Corcos <ccorcos@gmail.com>
2025-06-05 16:11:08 -0700

    New property.

--- src/client/components/ui/demos/AirtableDemo.tsx
+++ src/client/components/ui/demos/AirtableDemo.tsx
@@ -1,11 +1,15 @@
-import React, { useLayoutEffect, useState } from "react"
+import { capitalize } from "lodash"
+import React, { useLayoutEffect, useRef, useState } from "react"
 import { randomId } from "../../../../shared/randomId"
 import { useGet, useList, useWrite } from "../../../hooks/useDatabase"
 import { Subspace } from "../../Subspace"
 import { Button } from "../Button"
+import { DropdownMenu } from "../DropdownMenu"
 import { Input } from "../Input"
 import { ContentLayout, Layout, LeftPanelLayout } from "../Layout"
 import { ListBox, ListItem, useListBox } from "../ListBox"
+import { MenuItem } from "../MenuItem"
+import { Popup } from "../Popup"
 
 export function AirtableDemo() {
 	return (
@@ -66,43 +70,6 @@ function Airtable() {
 	)
 }
 
-function TableEditor(props: { tableId: string }) {
-	const { tableId } = props
-
-	const { localResult } = useGet(["table", tableId])
-	if (localResult.miss) return <div>Loading...</div>
-	const table = localResult.hit as Table
-
-	const write = useWrite()
-	const setTable = (table: Table) => {
-		write({ set: [{ key: ["table", table.id], value: table }] })
-	}
-
-	return (
-		<div>
-			<Input
-				value={table.name || ""}
-				onChange={(e) => setTable({ ...table, name: e.target.value })}
-			/>
-			<div>
-				{table.properties?.map((property) => (
-					<div key={property.id}>{property.name || "Untitled"}</div>
-				))}
-			</div>
-			<Button
-				onClick={() =>
-					setTable({
-						...table,
-						properties: [...(table.properties || []), { id: randomId(), type: { type: "string" } }],
-					})
-				}
-			>
-				New Property
-			</Button>
-		</div>
-	)
-}
-
 function TableList(props: {
 	tables: Table[]
 	selected: string[]
@@ -145,5 +112,90 @@ function TableList(props: {
 				<Button onClick={handleNewTable}>New Table</Button>
 			</div>
 		</ListBox>
+	)
+}
+
+function TableEditor(props: { tableId: string }) {
+	const { tableId } = props
+
+	const { localResult } = useGet(["table", tableId])
+	if (localResult.miss) return <div>Loading...</div>
+	const table = localResult.hit as Table
+
+	const write = useWrite()
+	const setTable = (table: Table) => {
+		write({ set: [{ key: ["table", table.id], value: table }] })
+	}
+
+	return (
+		<div>
+			<Input
+				value={table.name || ""}
+				onChange={(e) => setTable({ ...table, name: e.target.value })}
+			/>
+			<div>
+				{table.properties?.map((property) => (
+					<div key={property.id}>{property.name || "Untitled"}</div>
+				))}
+			</div>
+			<NewPropertyButton
+				onNewProperty={(type) => {
+					const properties = table.properties || []
+					const newProperty: Property = {
+						id: randomId(),
+						type,
+						name: capitalize(type.type === "list" ? type.item.type + "s" : type.type),
+					}
+					setTable({
+						...table,
+						properties: [...properties, newProperty],
+					})
+				}}
+			/>
+		</div>
+	)
+}
+
+const propertyTypes: PropertyType[] = [
+	{ type: "string" },
+	{ type: "number" },
+	{ type: "boolean" },
+	{ type: "relation" },
+	{ type: "list", item: { type: "string" } },
+	{ type: "list", item: { type: "number" } },
+	{ type: "list", item: { type: "boolean" } },
+	{ type: "list", item: { type: "relation" } },
+]
+
+function NewPropertyButton(props: { onNewProperty: (type: PropertyType) => void }) {
+	const buttonRef = useRef<HTMLButtonElement>(null)
+	const [open, setOpen] = useState(false)
+
+	const handleDismiss = () => {
+		setOpen(false)
+		buttonRef.current?.focus()
+	}
+
+	return (
+		<>
+			<Button ref={buttonRef} onClick={() => setOpen(true)}>
+				New Property
+			</Button>
+			<Popup open={open} anchor={buttonRef.current} onDismiss={handleDismiss}>
+				<DropdownMenu style={{ minWidth: 120 }}>
+					{propertyTypes.map((type, i) => (
+						<MenuItem
+							key={i}
+							onClick={() => {
+								handleDismiss()
+								props.onNewProperty(type)
+							}}
+						>
+							{capitalize(type.type === "list" ? `List of ${type.item.type}` : type.type)}
+						</MenuItem>
+					))}
+				</DropdownMenu>
+			</Popup>
+		</>
 	)
 }
