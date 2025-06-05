5026f63046f5d28a2b69a1e1d2ebe4622d2732f1
Chet Corcos <ccorcos@gmail.com>
2025-06-05 16:00:21 -0700

    Airtable WIP.

--- src/client/components/ui/demos/AirtableDemo.tsx
+++ src/client/components/ui/demos/AirtableDemo.tsx
@@ -1,44 +1,50 @@
 import React, { useLayoutEffect, useState } from "react"
-import {
-	applyRecordDbOperation,
-	recordDb,
-	RecordDbOperation,
-	TableDef,
-} from "../../../../shared/database/RecordDb"
-import { tupleTx } from "../../../../shared/database/TupleDb"
-import * as t from "../../../../shared/DataType"
 import { randomId } from "../../../../shared/randomId"
-import { useGet, useList } from "../../../hooks/useDatabase"
-import { useClientEnvironment } from "../../../services/ClientEnvironment"
+import { useGet, useList, useWrite } from "../../../hooks/useDatabase"
 import { Subspace } from "../../Subspace"
 import { Button } from "../Button"
-import { ComboBoxSelect } from "../ComboBox"
 import { Input } from "../Input"
 import { ContentLayout, Layout, LeftPanelLayout } from "../Layout"
 import { ListBox, ListItem, useListBox } from "../ListBox"
 
 export function AirtableDemo() {
 	return (
-		<Subspace prefix={["AirtableDemo2"]}>
+		<Subspace prefix={["AirtableDemo4"]}>
 			<Airtable />
 		</Subspace>
 	)
 }
 
+type PrimitivePropertyType =
+	| { type: "string" }
+	| { type: "number" }
+	| { type: "boolean" }
+	| { type: "relation" }
+
+type PropertyType = PrimitivePropertyType | { type: "list"; item: PrimitivePropertyType }
+
+type Property = { id: string; name?: string; type: PropertyType }
+
+type Table = {
+	id: string
+	name?: string
+	properties?: Property[]
+}
+
 function Airtable() {
 	const { localResult } = useList({
-		gt: ["model", "table"],
-		lt: ["model", "table", null],
+		gt: ["table"],
+		lt: ["table", null],
 	})
 
 	const result = localResult.hit || localResult.prefix
-	const tables = result?.map((item) => item.value as TableDef)
+	const tables = result?.map((item) => item.value as Table)
 
-	const [selected, setSelected] = useState<TableDef[]>([])
+	const [selected, setSelected] = useState<string[]>([])
 
 	// Annoying how this creates an extra render.
 	useLayoutEffect(() => {
-		if (tables && tables.length > 0) setSelected([tables[0]])
+		if (tables && tables.length > 0) setSelected([tables[0].id])
 	}, [Boolean(tables)])
 
 	return (
@@ -54,62 +60,69 @@ function Airtable() {
 			}
 		>
 			<ContentLayout style={{ padding: 8 }}>
-				{selected.length > 0 && <TableEditor table={selected[0]} />}
+				{selected.length > 0 && <TableEditor tableId={selected[0]} />}
 			</ContentLayout>
 		</Layout>
 	)
 }
 
-function TableEditor(props: { table: TableDef }) {
-	const { table } = props
-	// TODO: dataType editor.
-	return <div>{JSON.stringify(table, null, 2)}</div>
-}
+function TableEditor(props: { tableId: string }) {
+	const { tableId } = props
 
-function useWriteRecordDb() {
-	const { api, cache } = useClientEnvironment()
+	const { localResult } = useGet(["table", tableId])
+	if (localResult.miss) return <div>Loading...</div>
+	const table = localResult.hit as Table
 
-	return async (operations: RecordDbOperation[]) => {
-		// Optimistic update.
-		let finalize: any
-		const tx = tupleTx({
-			compare: cache.compare,
-			list: cache.listRaw,
-			write: (args) => {
-				console.log("write here", args)
-				finalize = cache.write(args)
-			},
-		})
-		const rdb = recordDb(tx)
-		for (const operation of operations) applyRecordDbOperation(rdb, operation)
-		tx.commit()
-
-		const promise = api.writeRecordDb({ operations })
-		promise.then(finalize)
-		return await promise
+	const write = useWrite()
+	const setTable = (table: Table) => {
+		write({ set: [{ key: ["table", table.id], value: table }] })
 	}
+
+	return (
+		<div>
+			<Input
+				value={table.name || ""}
+				onChange={(e) => setTable({ ...table, name: e.target.value })}
+			/>
+			<div>
+				{table.properties?.map((property) => (
+					<div key={property.id}>{property.name || "Untitled"}</div>
+				))}
+			</div>
+			<Button
+				onClick={() =>
+					setTable({
+						...table,
+						properties: [...(table.properties || []), { id: randomId(), type: { type: "string" } }],
+					})
+				}
+			>
+				New Property
+			</Button>
+		</div>
+	)
 }
 
 function TableList(props: {
-	tables: TableDef[]
-	selected: TableDef[]
-	setSelected: (selected: TableDef[]) => void
+	tables: Table[]
+	selected: string[]
+	setSelected: (selected: string[]) => void
 }) {
 	const { tables, selected, setSelected } = props
 
 	const { onClick, onKeyDown } = useListBox({
-		list: tables,
+		list: tables.map((table) => table.id),
 		selected,
 		setSelected,
 		multiselect: true,
 	})
 
-	const write = useWriteRecordDb()
+	const write = useWrite()
 
 	const handleNewTable = () => {
-		const table: TableDef = { table: "Untitled " + randomId(), dataType: t.any }
-		write([{ type: "setTable", args: table }])
-		setSelected([table])
+		const table: Table = { id: randomId() }
+		write({ set: [{ key: ["table", table.id], value: table }] })
+		setSelected([table.id])
 	}
 
 	return (
@@ -120,111 +133,17 @@ function TableList(props: {
 		>
 			{tables.map((table) => (
 				<ListItem
-					key={table.table}
+					key={table.id}
 					item={table}
-					selected={selected.includes(table)}
+					selected={selected.includes(table.id)}
 					style={{ padding: 4, borderRadius: 4 }}
 				>
-					{table.table.startsWith("Untitled ") ? (
-						<span style={{ color: "var(--fg1)" }}>Untitled</span>
-					) : (
-						table.table
-					)}
+					{table.name || "Untitled"}
 				</ListItem>
 			))}
 			<div>
 				<Button onClick={handleNewTable}>New Table</Button>
 			</div>
 		</ListBox>
-	)
-}
-
-export function RecordDbDemoOld() {
-	const { api, cache } = useClientEnvironment()
-
-	const [draft, setDraft] = useState<{ name: string; age: number }>({ name: "", age: -1 })
-	const [state, setState] = useState<"byId" | "byName" | "byAge">("byId")
-	const suffix = state === "byId" ? "" : "." + state
-	const { localResult, remoteResult } = useList({
-		gt: ["data", "person" + suffix],
-		lt: ["data", "person" + suffix, null],
-	})
-
-	const result = localResult.hit || localResult.prefix
-	// if (!result) throw remoteResult.promise
-
-	return (
-		<div>
-			<div>People</div>
-			<div style={{ display: "flex", gap: 8, flexDirection: "column", width: 200 }}>
-				<div>New Person</div>
-				<Input
-					placeholder="name"
-					value={draft.name}
-					onChange={(e) => setDraft({ ...draft, name: e.target.value })}
-				/>
-				<Input
-					type="number"
-					placeholder="age"
-					value={draft.age === -1 ? "" : draft.age}
-					onChange={(e) => setDraft({ ...draft, age: parseInt(e.target.value) })}
-				/>
-				<Button
-					onClick={() => {
-						const { name, age } = draft
-						if (!name) return
-						if (!age) return
-
-						let finalize: any
-						const tx = tupleTx({
-							compare: cache.compare,
-							list: cache.listRaw,
-							write: (args) => {
-								finalize = cache.write(args)
-							},
-						})
-						const rdb = recordDb(tx)
-
-						const record = { table: "person", id: randomId(), name, age }
-						rdb.setRecord(record)
-						tx.commit()
-						setDraft({ name: "", age: -1 })
-
-						const promise = api.writeRecordDb({ operations: [{ type: "setRecord", args: record }] })
-						promise.then(finalize)
-					}}
-				>
-					Create
-				</Button>
-			</div>
-
-			<ComboBoxSelect
-				items={["byId", "byName", "byAge"]}
-				value={state}
-				onChange={(x) => setState((x as any) ?? "byId")}
-				placeholder="Sort by..."
-			/>
-
-			<div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gap: "8px" }}>
-				{!result ? (
-					<div>Loading...</div>
-				) : (
-					result.map(({ key }) => <Person id={key.at(-1)!} key={key.at(-1)!} />)
-				)}
-			</div>
-		</div>
-	)
-}
-
-function Person(props: { id: string }) {
-	const { localResult, remoteResult } = useGet(["data", "person", props.id])
-	if (!localResult.hit) throw remoteResult.promise
-	const person = localResult.hit
-	return (
-		<React.Fragment>
-			<div>id: {person.id}</div>
-			<div>name: {person.name}</div>
-			<div>age: {person.age}</div>
-		</React.Fragment>
 	)
 }
