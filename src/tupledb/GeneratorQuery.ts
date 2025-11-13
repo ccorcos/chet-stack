eb6b82cc81a73d424cda38de2882745103c7d2e0
Chet Corcos <ccorcos@gmail.com>
2025-11-13 12:26:32 -0800

    Generator Query more IP.

--- src/tupledb/GeneratorQuery.ts
+++ src/tupledb/GeneratorQuery.ts
@@ -1,59 +1,45 @@
 import { InMemoryBaseOKV } from "./InMemoryBaseOKV"
-import { ListArgs, WriteArgs } from "./types"
-
-export type SyncBaseOKV<K, V> = {
-	list(args?: ListArgs<K>): { key: K; value: V }[]
-	write: (tx: WriteArgs<K, V>) => void
-}
-
-export type AsyncBaseOKV<K, V> = {
-	list(args?: ListArgs<K>): Promise<{ key: K; value: V }[]>
-	write: (tx: WriteArgs<K, V>) => Promise<void>
-}
-
-type QueryOp<K, V> = {
-	[F in keyof SyncBaseOKV<K, V>]: { fn: F; args: Parameters<SyncBaseOKV<K, V>[F]> }
-}[keyof SyncBaseOKV<K, V>]
+import { BaseOKV } from "./types"
 
 type AnyFunction = (...args: any[]) => any
-type FnObject = { [key: string]: AnyFunction }
-type GenObject<T extends FnObject> = {
-	[K in keyof T]: (
-		...args: Parameters<T[K]>
-	) => Generator<
-		{ fn: K; args: Parameters<T[K]> },
-		ReturnType<Awaited<T[K]>>,
-		ReturnType<Awaited<T[K]>>
-	>
+
+function* one<T>(value: T): Generator<any, Awaited<T>, any> {
+	const result = yield value
+	return result as Awaited<T>
 }
 
-function gen<T extends FnObject>(obj: T) {
-	// TODO: use a proxy instead.
-	const result: any = {}
-	for (const key in obj) {
-		const gen = function* (args) {
-			const result = yield* obj[key](...args)
-			return result
-		}
-		result[key] = gen
-	}
-	return result as GenObject<T>
-}
-
-const okv: SyncBaseOKV<string, any> = new InMemoryBaseOKV<string, any>()
-const tx = gen(okv)
-
-/** The Promise.all analog for generators. */
-function* all<T>(args: Array<Generator<any, T, any>>): Generator<any, T[], any> {
-	const results: T[] = []
-	for (const arg of args) {
-		const result = yield* arg
-		results.push(result)
-	}
+function* all<T>(args: Array<Generator<any, T, any>>): Generator<any, Awaited<T>[], any> {
+	const results = yield args
 	return results
 }
 
-function* scoreboard(tx: GenObject<SyncBaseOKV<string, any>>, limit: number) {
+type YieldedFn<T extends (...args: any[]) => any> = (
+	...args: Parameters<T>
+) => Generator<any, Awaited<ReturnType<T>>, any>
+
+type Yielded<T> = {
+	[K in keyof T]: T[K] extends (...args: any[]) => any ? YieldedFn<T[K]> : T[K]
+}
+
+function yieldedFns<T extends object>(obj: T): Yielded<T> {
+	return new Proxy(obj, {
+		get(target, prop) {
+			const original = target[prop as keyof T]
+			if (typeof original === "function") {
+				return function* (...args: any[]) {
+					const result = yield* one((original as AnyFunction).apply(target, args))
+					return result
+				}
+			}
+			return original
+		},
+	}) as Yielded<T>
+}
+
+const okv = new InMemoryBaseOKV<string, any>()
+const tx = yieldedFns(okv)
+
+function* scoreboard(tx: Yielded<BaseOKV<string, any>>, limit: number) {
 	const scores = yield* tx.list({ limit })
 	const results = yield* all(
 		scores.map(function* ({ key, value: { userId, score } }) {
@@ -64,22 +50,23 @@ function* scoreboard(tx: GenObject<SyncBaseOKV<string, any>>, limit: number) {
 	return results
 }
 
-function runSync<T>(db: SyncBaseOKV<string, any>, gen: Generator<any, T, any>): T {
+function run<T>(gen: Generator<any, T, any>): T {
 	let step = gen.next()
 	while (!step.done) {
-		const op = step.value
-		const out = db[op.fn](...op.args)
-		step = gen.next(out)
-	}
-	return step.value
-}
-
-async function runAsync<T>(db: AsyncBaseOKV<string, any>, gen: Generator<any, T, any>): Promise<T> {
-	let step = gen.next()
-	while (!step.done) {
-		const op = step.value
-		const out = await db[op.fn](...op.args)
-		step = gen.next(out)
+		if (step.value instanceof Promise) {
+			// Once we hit a promise, upgrade to async mode
+			return (async () => {
+				let result = await step.value
+				step = gen.next(result)
+				while (!step.done) {
+					result = await step.value
+					step = gen.next(result)
+				}
+				return step.value
+			})() as T
+		} else {
+			step = gen.next(step.value)
+		}
 	}
 	return step.value
 }
