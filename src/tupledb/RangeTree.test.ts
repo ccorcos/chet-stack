38460d464cf21d3d78484f3be7f7c78f11496318
Chet Corcos <ccorcos@gmail.com>
2025-12-03 11:44:24 -0800

    Range tree basics.

---
+++ src/tupledb/RangeTree.test.ts
@@ -0,0 +1,70 @@
+import { describe, it } from "mocha"
+import { strict as assert } from "node:assert"
+import { RangeTree } from "./RangeTree"
+
+describe("RangeTree", () => {
+	it("overlap", () => {
+		const tree = new RangeTree<number, number, number>()
+
+		const items = [
+			{ range: { gt: 0, lt: 10 }, key: 1, value: 1 },
+			{ range: { gt: 5, lt: 15 }, key: 2, value: 2 },
+			{ range: { gt: 10, lt: 20 }, key: 3, value: 3 },
+			{ range: { gt: 15, lt: 25 }, key: 4, value: 4 },
+			{ range: { gt: 20, lt: 30 }, key: 5, value: 5 },
+		]
+		for (const item of items) tree.set(item)
+
+		// Everything.
+		assert.deepEqual(tree.overlap(), items)
+
+		// Basic overlap.
+		assert.deepEqual(tree.overlap({ gt: 1, lt: 11 }), items.slice(0, 3))
+
+		// No overlap left side.
+		assert.deepEqual(tree.overlap({ gt: -11, lt: -1 }), [])
+
+		// No overlap left side boundary
+		assert.deepEqual(tree.overlap({ gt: -11, lte: 0 }), [])
+
+		// A single point.
+		assert.deepEqual(tree.overlap({ gte: 10, lte: 10 }), items.slice(1, 2))
+		// NOTE: we have tests for compareRange in Range.test.ts so we don't need to be exhaustive here.
+	})
+
+	it("updates", () => {
+		const tree = new RangeTree<number, number, number>()
+
+		const items = [
+			{ range: { gt: 0, lt: 10 }, key: 1, value: 1 },
+			{ range: { gt: 5, lt: 15 }, key: 2, value: 2 },
+			{ range: { gt: 10, lt: 20 }, key: 3, value: 3 },
+			{ range: { gt: 15, lt: 25 }, key: 4, value: 4 },
+			{ range: { gt: 20, lt: 30 }, key: 5, value: 5 },
+		]
+		for (const item of items) tree.set(item)
+
+		// Update a value
+		tree.set({ range: { gt: 5, lt: 15 }, key: 2, value: 200 })
+		assert.deepEqual(tree.overlap({ gte: 10, lte: 10 }), [
+			{ range: { gt: 5, lt: 15 }, key: 2, value: 200 },
+		])
+
+		// Delete a value
+		tree.delete({ key: 2 })
+		assert.deepEqual(tree.overlap({ gte: 10, lte: 10 }), [])
+
+		// Reinsert the original value
+		tree.set({ range: { gt: 5, lt: 15 }, key: 2, value: 2 })
+		assert.deepEqual(tree.overlap({ gte: 10, lte: 10 }), [
+			{ range: { gt: 5, lt: 15 }, key: 2, value: 2 },
+		])
+
+		// Add another value at the same exact range with a different key
+		tree.set({ range: { gt: 5, lt: 15 }, key: -1, value: -1 })
+		assert.deepEqual(tree.overlap({ gte: 10, lte: 10 }), [
+			{ range: { gt: 5, lt: 15 }, key: -1, value: -1 },
+			{ range: { gt: 5, lt: 15 }, key: 2, value: 2 },
+		])
+	})
+})
---
+++ src/tupledb/RangeTree.ts
@@ -0,0 +1,61 @@
+// Goals...
+// - use and Okv for storing ranges
+// - reuse this in RangeEmitter
+// - reuse this in cache.ranged
+// - compute overlaps in various ways.
+
+import { compare } from "shared/compare"
+import { InMemoryOkv } from "./InMemoryOkv"
+import { compareRange, overlapsRange, Range } from "./Range"
+
+/**
+ * Ideas for later...
+ * - current a single unique key must have only one range. but we could change that...
+ * - can we store both the ranges and the inverse index in the same okv and make it durable?
+ * - can we make `overlaps` more efficient with a proper interval tree?
+ */
+export class RangeTree<B, K, V> {
+	ranges: InMemoryOkv<[Range<B>, K], V>
+
+	// Inverse index.
+	keys: InMemoryOkv<K, { range: Range<B>; value: V }>
+
+	constructor(
+		public compareBound: (a: B, b: B) => number = compare,
+		public compareKey: (a: K, b: K) => number = compare
+	) {
+		this.ranges = new InMemoryOkv<[Range<B>, K]>((a, b) => {
+			const d = compareRange(a[0], b[0], this.compareBound)
+			if (d !== 0) return d
+			return compareKey(a[1], b[1])
+		})
+		this.keys = new InMemoryOkv<K, { range: Range<B>; value: V }>(compareKey)
+	}
+
+	set = (args: { range: Range<B>; key: K; value: V }) => {
+		const { range, key, value } = args
+		this.ranges.write({ set: [{ key: [range, key], value }] })
+		this.keys.write({ set: [{ key, value: { range, value } }] })
+	}
+
+	delete = (args: { key: K; range?: Range<B> }) => {
+		let { key, range } = args
+		if (range === undefined) {
+			const result = this.keys.list({ gte: key, lte: key }).at(0)
+			if (result === undefined) return
+			range = result.value.range
+		}
+		this.ranges.write({ delete: [[range, key]] })
+		this.keys.write({ delete: [key] })
+	}
+
+	overlap = (range: Range<B> = {}) => {
+		const result: { range: Range<B>; key: K; value: V }[] = []
+		for (const { key, value } of this.ranges.list()) {
+			if (overlapsRange(key[0], range, this.compareBound)) {
+				result.push({ range: key[0], key: key[1], value })
+			}
+		}
+		return result
+	}
+}
