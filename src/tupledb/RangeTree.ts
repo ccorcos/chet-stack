73d2b54da1f28b6f87a7c53e54a8de24b344e2ea
Chet Corcos <ccorcos@gmail.com>
2025-12-03 13:05:47 -0800

    Bring back old RangeTree and rename to TupleRangeTree.

--- src/tupledb/RangeTree.ts
+++ src/tupledb/RangeTree.ts
@@ -1,58 +1,58 @@
-import { Compare } from "shared/compare"
-import { decodeRange, encodeRange, overlapsRange, Range } from "./Range"
-import { tupleDb, tupleOkv, tupleTx } from "./TupleDb"
-import { TupleOkv } from "./types"
+// Goals...
+// - use and Okv for storing ranges
+// - reuse this in RangeEmitter
+// - reuse this in cache.ranged
+// - compute overlaps in various ways.
+
+import { compare } from "shared/compare"
+import { InMemoryOkv } from "./InMemoryOkv"
+import { compareRange, overlapsRange, Range } from "./Range"
 
 /**
- * This assumes all boundaries can be compared in a tupleDb.
- *
  * Ideas for later...
+ * - current a single unique key must have only one range. but we could change that...
+ * - can we store both the ranges and the inverse index in the same okv and make it durable?
  * - can we make `overlaps` more efficient with a proper interval tree?
  */
 export class RangeTree<B, K, V> {
-	public db: TupleOkv
+	ranges: InMemoryOkv<[Range<B>, K], V>
+	// Inverse index.
+	keys: InMemoryOkv<K, { range: Range<B>; value: V }>
 
-	constructor(db?: TupleOkv) {
-		if (!db) this.db = tupleOkv()
-		else this.db = db
+	constructor(
+		public compareBound: (a: B, b: B) => number = compare,
+		public compareKey: (a: K, b: K) => number = compare
+	) {
+		this.ranges = new InMemoryOkv<[Range<B>, K]>((a, b) => {
+			const d = compareRange(a[0], b[0], this.compareBound)
+			if (d !== 0) return d
+			return compareKey(a[1], b[1])
+		})
+		this.keys = new InMemoryOkv<K, { range: Range<B>; value: V }>(compareKey)
 	}
 
 	set = (args: { range: Range<B>; key: K; value: V }) => {
 		const { range, key, value } = args
-		const tx = tupleTx(this.db)
-		tx.subspace(["ranges"]).set([encodeRange(range), key], value)
-		tx.subspace(["keys"]).set([key, encodeRange(range)], value)
-		tx.commit()
+		this.ranges.write({ set: [{ key: [range, key], value }] })
+		this.keys.write({ set: [{ key, value: { range, value } }] })
 	}
 
 	delete = (args: { key: K; range?: Range<B> }) => {
 		let { key, range } = args
-
-		// Delete all ranges.
 		if (range === undefined) {
-			const tx = tupleTx(this.db)
-			const results = tx.subspace(["keys"]).list({ gte: [key], lte: [key, null] })
-			for (const item of results) {
-				const [key, range] = item.key
-				tx.subspace(["ranges"]).delete([range, key])
-			}
-			tx.commit()
-			return
+			const result = this.keys.list({ gte: key, lte: key }).at(0)
+			if (result === undefined) return
+			range = result.value.range
 		}
-
-		// Delete just a single range.
-		const tx = tupleTx(this.db)
-		tx.subspace(["ranges"]).delete([encodeRange(range), key])
-		tx.subspace(["keys"]).delete([key, encodeRange(range)])
-		tx.commit()
+		this.ranges.write({ delete: [[range, key]] })
+		this.keys.write({ delete: [key] })
 	}
 
 	overlap = (range: Range<B> = {}) => {
 		const result: { range: Range<B>; key: K; value: V }[] = []
-		for (const { key, value } of tupleDb(this.db).subspace(["ranges"]).list()) {
-			const itemRange = decodeRange(key[0]) as Range<B>
-			if (overlapsRange(itemRange, range, this.db.compare as Compare<B>)) {
-				result.push({ range: itemRange, key: key[1], value })
+		for (const { key, value } of this.ranges.list()) {
+			if (overlapsRange(key[0], range, this.compareBound)) {
+				result.push({ range: key[0], key: key[1], value })
 			}
 		}
 		return result
